aa823bbb60fe8dd2a744ed0c4ee540096d1e8fc1
Elie Habib <elie.habib@gmail.com>
2026-02-19 12:28:41 +0400

    fix: exclude HTML from Workbox precache glob

--- vite.config.ts
+++ vite.config.ts
@@ -196,7 +196,7 @@ export default defineConfig({
       },
 
       workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
+        globPatterns: ['**/*.{js,css,ico,png,svg,woff2}'],
         globIgnores: ['**/ml-*.js', '**/onnx*.wasm'],
         navigateFallback: '/index.html',
         navigateFallbackDenylist: [/^\/api\//, /^\/settings/],
